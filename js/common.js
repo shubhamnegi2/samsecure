6dc828f9819a8cc8aa79c93842526c8dd4401491
shubham negi <shubhamnegi734@gmail.com>
2022-12-22 16:53:25 +0530

    initial commit
    initial commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+
+  $(".hamburgermenu").click(function () {
+    $(".nav").slideToggle();
+  });
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+      $(".headerContainer").addClass("headerBGColor");
+    } else {
+      $(".headerContainer").removeClass("headerBGColor");
+    }
+  });
